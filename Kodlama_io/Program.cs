f54a873607036a605b38820e68f028cba27691ab
Gökhan Aydın <aydingo18@itu.edu.tr>
2021-01-17 00:16:56 +0300

    Add project files.

---
+++ Kodlama_io/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kodlama_io
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
