a7d427586aee67942fbccf318eae74ab506a9542
Gökhan Aydın <aydingo18@itu.edu.tr>
2021-01-20 23:50:39 +0300

    20 Ocak Ders Sonu

---
+++ OOP1/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace OOP1
+{
+    // snippet
+    class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public double UnitPrice { get; set; }
+        public int UnitInStock { get; set; }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace OOP1
+{
+    class ProductManager
+    {
+        //encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName + " eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+
+        
+        }
+    }
+}
